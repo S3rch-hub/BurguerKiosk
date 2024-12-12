bacbcd79370bd2736a2937c11e96f69512688626
Sergio <sermmoba8@gmail.com>
2024-12-12 10:12:36 +0100

    Proyecto de ayer en NetBeans

---
+++ src/main/java/com/mycompany/burguerkiosk/BurguerKiosk.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.burguerkiosk;
+
+/**
+ *
+ * @author Sergio
+ */
+public class BurguerKiosk {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
